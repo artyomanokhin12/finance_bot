9c3588fb04157a53627038efbfc298e950958519
dudos <dudos1515@gmail.com>
2024-08-12 14:31:19 +0300

    Добавление sql

---
+++ db.sql
@@ -0,0 +1,26 @@
+INSERT INTO incomes (incomes_name)
+VALUES 
+    ('Зарплата'),
+    ('Подарки'),
+    ('В долг'),
+    ('Прочие');
+
+INSERT INTO spendings (spendings_name, optional_spending)
+VALUES
+    ('Кафе', 'false'),
+    ('Бар', 'false'),
+    ('Здоровье', 'false'),
+    ('Такси', 'false'),
+    ('Книги', 'false'),
+    ('Подписки', 'false'),
+    ('Развлечения', 'false'),
+    ('Алкоголь', 'false'),
+    ('Никотин', 'false'),
+    ('Концерты', 'false'),
+    ('Одежда', 'false'),
+    ('Долг', 'false'),
+    ('Прочие', 'false'),
+    ('Дом', 'true'),
+    ('Продукты', 'true'),
+    ('Транспорт', 'true'),
+    ('Связь и интернет', 'true');
